b9558acd525f07a55eec2ea2b881ef1c8eed33db
Chantel Lowe <chantellowe17@gmail.com>
2019-03-23 04:41:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,16 +5,6 @@
 
 
 def turn_count(board)
-
-  counter = 1
-  board.each do |spaces|
-    puts board.rindex("X")
-    # puts board.rindex("O")
-    counter += 1
-  end
-  
-  
-
 end
 
 
@@ -22,16 +12,7 @@ end
 
 
 def current_player
-
-  if counter % 2 == 0
-    puts "It is Player 1's turn (X)"
-  else
-    puts "It is Player 2's turn (O)"
-  end
-
 end
-
-# turn_count(board)
 
 
 
